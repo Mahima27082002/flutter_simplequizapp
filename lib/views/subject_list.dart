ae9ba95632d51360d35fc186a655be48f64b519a
Mahima27082002 <134771871+Mahima27082002@users.noreply.github.com>
2023-06-07 20:20:38 +0530

    Add files via upload

---
+++ lib/views/subject_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simplequizapp/views/quiz_screen.dart';
+
+class SubjectListScreen extends StatelessWidget {
+  const SubjectListScreen({super.key});
+
+  final List subjectListData = const [
+    {"subject_name": "Hindi", "subject_id": "1001"},
+    {"subject_name": "English", "subject_id": "1002"},
+    {"subject_name": "Science", "subject_id": "1003"},
+    {"subject_name": "Math", "subject_id": "1004"},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Subject List"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 12,
+            ),
+            ...subjectListData
+                .map(
+                  (e) => Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: ListTile(
+                        onTap: () {
+                          print(e['subject_id']);
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const QuizScreen(),
+                            ),
+                          );
+                        },
+                        tileColor: Colors.teal,
+                        textColor: Colors.white,
+                        iconColor: Colors.white,
+                        title: Text(
+                          e['subject_name'],
+                        ),
+                        trailing: const Icon(Icons.arrow_forward)),
+                  ),
+                )
+                .toList()
+          ],
+        ),
+      ),
+    );
+  }
+}
